791f96a062465bf55ffadd2b81a3384049222a12
Arshak Mkrtchyan <arshak.mkrtchyan@acba.am>
2023-12-08 10:16:46 +0400

    create textinputlayout component

--- app/src/main/java/am/acba/components/MainActivity.kt
+++ app/src/main/java/am/acba/components/MainActivity.kt
@@ -3,12 +3,17 @@ package am.acba.components
 import am.acba.component.button.PrimaryButton
 import am.acba.component.input.PrimaryInput
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
+import androidx.constraintlayout.widget.ConstraintHelper
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -44,6 +49,13 @@ class MainActivity : AppCompatActivity() {
                 this@MainActivity.findViewById<PrimaryInput>(R.id.input).apply {
                     isErrorEnabled = true
                     error = "Error"
+                }
+            }
+        }
+        findViewById<PrimaryButton>(R.id.buttonGhost).apply {
+            setOnClickListener {
+                this@MainActivity.findViewById<PrimaryInput>(R.id.input).apply {
+                    clearFocus()
                 }
             }
         }
