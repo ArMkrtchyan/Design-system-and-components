e6c94290d759b2f4d10e8db4609b331ef277ee57
Anna.Hovsepyan <Anna.HovsepyanM@acba.am>
2024-06-17 10:22:30 +0400

    Implement Alert component

--- app/src/main/java/am/acba/components/AlertsFragment.kt
+++ app/src/main/java/am/acba/components/AlertsFragment.kt
@@ -1,8 +1,10 @@
 package am.acba.components
 
+import am.acba.component.alert.PrimaryAlert
 import am.acba.components.base.BaseViewBindingFragment
 import am.acba.components.base.Inflater
 import am.acba.components.databinding.FragmentAlertsBinding
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class AlertsFragment : BaseViewBindingFragment<FragmentAlertsBinding>() {
@@ -12,7 +14,19 @@ class AlertsFragment : BaseViewBindingFragment<FragmentAlertsBinding>() {
         get() = mBinding.toolbar
 
     override fun FragmentAlertsBinding.initView() {
-
+        alert.apply {
+            setType(PrimaryAlert.AlertTypes.INFO)
+            setTitle("Title")
+            setBody("Lorem ipsum dolor sit amet consectetur. Integer odio consectetur interdum at nullam nunc adipiscing. Quis nec diam fames feugiat ac non.")
+            setLink("Link")
+            setOnCloseClickListener {
+                Toast.makeText(context, "Button clicked", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            setOnLinkClickListener {
+                Toast.makeText(context, "Link clicked", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
-
 }
