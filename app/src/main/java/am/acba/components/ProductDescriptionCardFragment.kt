58f05b096e2646f6be1b3fa4cfe96ed05398ad95
Arshak Mkrtchyan <arshak.mkrtchyan@acba.am>
2024-09-09 13:18:45 +0400

    create product card component

--- app/src/main/java/am/acba/components/ProductDescriptionCardFragment.kt
+++ app/src/main/java/am/acba/components/ProductDescriptionCardFragment.kt
@@ -46,7 +46,7 @@ class ProductDescriptionCardFragment : BaseViewBindingFragment<FragmentProductDe
     private fun setProductCard0() {
         mBinding.productCardFull.setTitle("Խնայողական")
         mBinding.productCardFull.setDescription("Հետևիր խնայողություններիդ աճին")
-        mBinding.productCardFull.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment_cropped))
+        mBinding.productCardFull.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment))
         mBinding.productCardFull.setBullets(list3)
         mBinding.productCardExcample.setBadgesGroup(list1)
         mBinding.productCardExcample.setBullets(list3)
@@ -55,28 +55,28 @@ class ProductDescriptionCardFragment : BaseViewBindingFragment<FragmentProductDe
     private fun setProductCard1() {
         mBinding.productCard1.setTitle("Խնայողական")
         mBinding.productCard1.setSubTitle("ԹՎԱՅԻՆ ՔԱՐՏ")
-        mBinding.productCard1.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment_cropped))
+        mBinding.productCard1.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment))
         mBinding.productCard1.setBadgesGroup(list1)
         mBinding.productCard1.setBullets(list3)
     }
 
     private fun setProductCard2() {
         mBinding.productCard2.setTitle("Խնայողական")
-        mBinding.productCard2.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment_cropped))
+        mBinding.productCard2.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment))
         mBinding.productCard2.setBadgesGroup(list1)
         mBinding.productCard2.setBullets(list3)
     }
 
     private fun setProductCard3() {
         mBinding.productCard3.setTitle("Խնայողական")
-        mBinding.productCard3.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment_cropped))
+        mBinding.productCard3.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment))
         mBinding.productCard3.setBadgesGroup(list2)
         mBinding.productCard3.setBullets(list3)
     }
 
     private fun setProductCard4() {
         mBinding.productCard4.setTitle("Խնայողական")
-        mBinding.productCard4.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment_cropped))
+        mBinding.productCard4.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment))
         mBinding.productCard4.setBadgesGroup(list2)
         mBinding.productCard4.setBullets(list4)
     }
@@ -84,13 +84,13 @@ class ProductDescriptionCardFragment : BaseViewBindingFragment<FragmentProductDe
     private fun setProductCard5() {
         mBinding.productCard5.setTitle("Խնայողական")
         mBinding.productCard5.setSubDescription("Հասանելի 7 արժույթagxyagxgaxgaxajhcahjcahjgxahjgcahjchajcahach svcavchjsvhjcshcbshjcbshjcbshjbcsjkbcksbs")
-        mBinding.productCard5.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment_cropped))
+        mBinding.productCard5.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment))
         mBinding.productCard5.setBadgesGroup(list2)
     }
 
     private fun setProductCard6() {
         mBinding.productCard6.setTitle("Խնայողական Խնայողական Խնայողական Խնայողական Խնայողական Խնայողական Խնայողական")
-        mBinding.productCard6.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment_cropped))
+        mBinding.productCard6.setImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.investment))
         mBinding.productCard6.setBadgesGroup(list2)
         mBinding.productCard6.setBullets(list3)
     }
