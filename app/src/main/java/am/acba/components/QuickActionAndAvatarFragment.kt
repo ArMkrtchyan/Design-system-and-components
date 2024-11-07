b7a2a8c519d74b9e2a581f6953a7ef95dbd6b535
Misak.Manukyan <misak.manukyan@acba.am>
2024-11-07 17:24:57 +0400

    add functions for action badge background icon and tint

--- app/src/main/java/am/acba/components/QuickActionAndAvatarFragment.kt
+++ app/src/main/java/am/acba/components/QuickActionAndAvatarFragment.kt
@@ -1,10 +1,14 @@
 package am.acba.components
 
+import am.acba.component.extensions.getColorStateListFromAttr
 import am.acba.component.toolbar.PrimaryToolbar
 import am.acba.components.base.BaseViewBindingFragment
 import am.acba.components.base.Inflater
 import am.acba.components.databinding.FragmentQuickActionAndAvatarBinding
+import android.annotation.SuppressLint
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources.getColorStateList
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.core.content.ContextCompat
 
 class QuickActionAndAvatarFragment : BaseViewBindingFragment<FragmentQuickActionAndAvatarBinding>() {
@@ -15,9 +19,12 @@ class QuickActionAndAvatarFragment : BaseViewBindingFragment<FragmentQuickAction
 
     override fun FragmentQuickActionAndAvatarBinding.initView() {
         mBinding.quickAction.setAnimation("check_test.json", am.acba.component.R.attr.contentWarning)
-        mBinding.quickAction.setActionBudgeImage(am.acba.component.R.drawable.ic_add)
+        mBinding.quickAction.setActionBadgeImage(ContextCompat.getDrawable(requireContext(), am.acba.component.R.drawable.ic_success_filled))
         mBinding.quickAction.setOnActionBadgeClickListener {
             Toast.makeText(requireContext(), "Clicked", Toast.LENGTH_SHORT).show()
         }
+        mBinding.quickAction.setActionBadgeBackground(am.acba.component.R.drawable.background_rounded)
+        mBinding.quickAction.setActionBadgeImageTint(context?.getColorStateListFromAttr(am.acba.component.R.attr.contentBrand))
+        mBinding.quickAction.setActionBadgeImage(getDrawable(requireContext(), am.acba.component.R.drawable.ic_right))
     }
 }
