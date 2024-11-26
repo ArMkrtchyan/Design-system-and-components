cf15d5fb6b62b34395970e648379b7ed638c402e
Misak.Manukyan <misak.manukyan@acba.am>
2024-11-26 18:10:45 +0400

    fix preventing dot input in middle of text
    (cherry picked from commit 6645a54a2de838c5931f88919783b0eeab501b5b)

--- component/src/main/java/am/acba/component/input/PrimaryEditText.kt
+++ component/src/main/java/am/acba/component/input/PrimaryEditText.kt
@@ -7,25 +7,20 @@ import android.util.Log
 import com.google.android.material.textfield.TextInputEditText
 
 class PrimaryEditText : TextInputEditText {
-    private var disableDot = false
+    var disableDot = false
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     override fun onSelectionChanged(selStart: Int, selEnd: Int) {
         if (disableDot && selStart == selEnd) {
-            val unformattedText = text?.toString()?.replace(",", "") ?: ""
-            keyListener = if (selStart < (unformattedText.length)) {
-                DigitsKeyListener.getInstance("0123456789")
+            if (selStart < (text?.length ?: 1)) {
+                keyListener = DigitsKeyListener.getInstance("0123456789")
             } else {
-                DigitsKeyListener.getInstance("0123456789.")
+                keyListener = DigitsKeyListener.getInstance("0123456789.")
             }
         }
         Log.d("SelectionChanged", "selStart: $selStart, selEnd: $selEnd")
         super.onSelectionChanged(selStart, selEnd)
-    }
-
-    fun setDisableDot(isDotDisabled: Boolean) {
-        disableDot = isDotDisabled
     }
 }
