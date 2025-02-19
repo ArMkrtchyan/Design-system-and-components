45f07150327a977f29c623f6f852a82748004aae
Anna.Hovsepyan <Anna.HovsepyanM@acba.am>
2025-02-19 16:17:22 +0400

    Add email validation function for PrimaryInput

--- component/src/main/java/am/acba/component/input/PrimaryInput.kt
+++ component/src/main/java/am/acba/component/input/PrimaryInput.kt
@@ -323,6 +323,8 @@ open class PrimaryInput : TextInputLayout {
         }
     }
 
+    fun isValidEmail(text: String) = android.util.Patterns.EMAIL_ADDRESS.matcher(text).matches()
+
     companion object {
         const val SHAKE_AMPLITUDE = 500L
         const val VIBRATION_AMPLITUDE = 80L
