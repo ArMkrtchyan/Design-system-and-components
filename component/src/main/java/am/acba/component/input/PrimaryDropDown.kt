d704b572f0ea35250b8485373b897fc40fc292f0
Vahe.Petrosyan <Vahe.Petrosyan@acba.am>
2025-04-21 14:33:08 +0400

    save input and drop down  states

--- component/src/main/java/am/acba/component/input/PrimaryDropDown.kt
+++ component/src/main/java/am/acba/component/input/PrimaryDropDown.kt
@@ -4,9 +4,15 @@ import am.acba.component.R
 import am.acba.component.databinding.PrimaryDropdownBinding
 import am.acba.component.extensions.getColorStateListFromAttr
 import am.acba.component.extensions.inflater
+import am.acba.component.extensions.restoreChildViewStates
+import am.acba.component.extensions.saveChildViewStates
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
 import android.util.AttributeSet
+import android.util.SparseArray
 import android.widget.FrameLayout
 import androidx.annotation.DrawableRes
 
@@ -89,5 +95,43 @@ open class PrimaryDropDown @JvmOverloads constructor(
         binding.frame.isClickable = isEnabled
         binding.inputDropDown.isFocusable = isEnabled
         binding.inputDropDown.isClickable = isEnabled
+    }
+    companion object{
+        const val SUPER_STATE_KEY = "SUPER_STATE_KEY"
+        const val SPARSE_STATE_KEY = "SPARSE_STATE_KEY"
+    }
+    override fun dispatchSaveInstanceState(container: SparseArray<Parcelable>) {
+        dispatchFreezeSelfOnly(container)
+    }
+
+    override fun dispatchRestoreInstanceState(container: SparseArray<Parcelable>) {
+        dispatchThawSelfOnly(container)
+    }
+
+    override fun onSaveInstanceState(): Parcelable {
+        return Bundle().apply {
+            putParcelable(SUPER_STATE_KEY, super.onSaveInstanceState())
+            putSparseParcelableArray(SPARSE_STATE_KEY, saveChildViewStates())
+        }
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        var newState = state
+        if (newState is Bundle) {
+            val childrenState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                newState.getSparseParcelableArray(SPARSE_STATE_KEY, Parcelable::class.java)
+            } else {
+                newState.getSparseParcelableArray(SPARSE_STATE_KEY)
+            }
+            childrenState?.let { restoreChildViewStates(it) }
+            newState = newState.getUsCoParcelable(SUPER_STATE_KEY)
+        }
+        super.onRestoreInstanceState(newState)
+    }
+
+    inline fun <reified T : Parcelable> Bundle.getUsCoParcelable(key: String?) = if (Build.VERSION.SDK_INT >= 33) {
+        this.getParcelable(key, T::class.java)
+    } else {
+        this.getParcelable(key)
     }
 }
