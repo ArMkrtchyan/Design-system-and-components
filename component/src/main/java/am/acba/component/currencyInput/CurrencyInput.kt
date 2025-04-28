17b60988025ff426fb2927e3de66c4d3c93c7868
Anna.Hovsepyan <Anna.HovsepyanM@acba.am>
2025-04-28 12:33:53 +0400

    Fix empty currency input text length issue

--- component/src/main/java/am/acba/component/currencyInput/CurrencyInput.kt
+++ component/src/main/java/am/acba/component/currencyInput/CurrencyInput.kt
@@ -543,7 +543,9 @@ class CurrencyInput @JvmOverloads constructor(
                 .takeIf { it.isNotEmpty() }
                 ?.let { if (formattingWithOutDot) it.numberFormattingWithOutDot() else it.numberFormatting() } ?: ""
 
-            setMaxLengthForFormattedText(formattedText.length)
+            val textLength = if (formattedText.length <= 0) 1 else formattedText.length
+            setMaxLengthForFormattedText(textLength)
+
             binding.amount.editText?.editableText?.replace(0, binding.amount.editText?.editableText?.length ?: 0, formattedText)
         }
     }
