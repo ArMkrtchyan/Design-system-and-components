8807ff6e17c226fa7d3c9bf5728debffc0cc3c33
Anna.Hovsepyan <Anna.HovsepyanM@acba.am>
2025-04-28 11:56:03 +0400

    Implement file upload

---
+++ component/src/main/java/am/acba/component/fileUpload/FileUploadAdapter.kt
@@ -0,0 +1,120 @@
+package am.acba.component.fileUpload
+
+import am.acba.component.R
+import am.acba.component.databinding.ItemFileUploadBinding
+import am.acba.component.extensions.getColorStateListFromAttr
+import am.acba.component.extensions.inflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class FileUploadAdapter() : ListAdapter<FileUploadModel, FileUploadAdapter.ViewHolder>(FileUploadDiffCallBack()) {
+
+    private var spanCount = 2
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemFileUploadBinding.inflate(parent.context.inflater(), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    inner class ViewHolder(private val binding: ItemFileUploadBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun onBind(model: FileUploadModel) {
+            binding.fileUpload.apply {
+                setTitle(model.title)
+                setBody(model.body)
+
+                emptyIconDrawable = model.emptyIcon
+                emptyIconTint = model.emptyIconTint ?: context.getColorStateListFromAttr(R.attr.contentSecondary)
+                emptyIconBackground = model.emptyIconBackground
+                emptyIconBackgroundTint = model.emptyIconBackgroundTint
+
+                uploadedIconDrawable = model.uploadedIcon
+                uploadedIconTint = model.uploadedIconTint ?: context.getColorStateListFromAttr(R.attr.contentSecondary)
+                uploadedIconBackground = model.uploadedIconBackground
+                uploadedIconBackgroundTint = model.uploadedIconBackgroundTint
+                setupFileUploadState(model)
+
+                model.description?.let { setDescription(it) } ?: setDescriptionVisibility(false)
+
+                model.fileDeleteDialogTitle?.let { fileDeleteDialogTitle = it }
+                model.fileDeleteDialogDescription?.let { fileDeleteDialogDescription = it }
+                model.fileDeleteDialogPositiveButtonText?.let { fileDeleteDialogPositiveButtonText = it }
+                model.fileDeleteDialogNegativeButtonText?.let { fileDeleteDialogNegativeButtonText = it }
+
+                fileDeleteDialogClickListener = { resetEmptyStateAfterDeletion(model) }
+                setOnItemClickListener { model.itemClickListener?.invoke(it, model) }
+            }
+        }
+
+        private fun setupFileUploadState(model: FileUploadModel) {
+            binding.fileUpload.apply {
+                if (model.fileUploadState == FileUpload.FileUploadState.UPLOADING) {
+                    setFileUploadState(model.fileUploadState) {
+                        model.uploadedImage?.let { setUploadedImage(context, it) }
+                        model.uploadedFile?.let { setUploadedFile(context, it) }
+                        model.fileUploadState = fileUploadState
+                    }
+                } else {
+                    setFileUploadState(model.fileUploadState)
+                }
+            }
+        }
+
+        private fun resetEmptyStateAfterDeletion(model: FileUploadModel) {
+            val updatedList = currentList.map {
+                if (it.id == model.id) {
+                    it.copy(fileUploadState = FileUpload.FileUploadState.EMPTY, description = null)
+                } else {
+                    it.copy()
+                }
+            }
+            submitList(updatedList)
+            model.fileDeleteDialogClickListener?.invoke(model)
+        }
+    }
+
+    fun setSpanCount(spanCount: Int) {
+        this.spanCount = spanCount
+    }
+
+    override fun onCurrentListChanged(previousList: List<FileUploadModel>, currentList: List<FileUploadModel>) {
+        super.onCurrentListChanged(previousList, currentList)
+
+        if (spanCount != 2) return
+
+        currentList.chunked(2).forEach { pair ->
+            if (pair.size == 1) return
+
+            val (first, second) = pair
+            val isVisible = !first.description.isNullOrEmpty() || !second.description.isNullOrEmpty()
+            if (isVisible) {
+                if (first.description == null) first.description = " "
+                if (second.description == null) second.description = " "
+            }
+        }
+    }
+
+    private class FileUploadDiffCallBack : DiffUtil.ItemCallback<FileUploadModel>() {
+
+        override fun areItemsTheSame(oldItem: FileUploadModel, newItem: FileUploadModel) = oldItem.id == newItem.id
+            && oldItem.fileUploadState == newItem.fileUploadState
+            && oldItem.title == newItem.title
+            && oldItem.description == newItem.description
+            && oldItem.uploadedImage == newItem.uploadedImage
+            && oldItem.uploadedFile == newItem.uploadedFile
+
+        override fun areContentsTheSame(oldItem: FileUploadModel, newItem: FileUploadModel) = oldItem.id == newItem.id
+            && oldItem.fileUploadState == newItem.fileUploadState
+            && oldItem.title == newItem.title
+            && oldItem.description == newItem.description
+            && oldItem.uploadedImage == newItem.uploadedImage
+            && oldItem.uploadedFile == newItem.uploadedFile
+    }
+}
---
+++ component/src/main/java/am/acba/component/fileUpload/FileUploadList.kt
@@ -0,0 +1,88 @@
+﻿package am.acba.component.fileUpload
+
+import am.acba.component.R
+import am.acba.component.databinding.FileUploadListBinding
+import am.acba.component.extensions.dpToPx
+import am.acba.component.extensions.inflater
+import am.acba.component.helpers.GridItemDecorator
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.GridLayoutManager
+
+class FileUploadList : FrameLayout {
+    private val binding by lazy {
+        FileUploadListBinding.inflate(
+            context.inflater(),
+            this,
+            false
+        )
+    }
+
+    private val fileUploadAdapter by lazy { FileUploadAdapter() }
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet) {
+        context.obtainStyledAttributes(attrs, R.styleable.FileUploadList).apply {
+            addView(binding.root)
+            try {
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            recycle()
+            invalidate()
+        }
+    }
+
+    fun initList(list: List<FileUploadModel>, spanCount: Int = 2) {
+        binding.rvFileUpload.apply {
+            isVisible = list.isNotEmpty()
+            if (!isVisible) return
+
+            val gridLayoutManager = GridLayoutManager(context, spanCount)
+            addItemDecoration(GridItemDecorator(spanCount, 8.dpToPx()))
+            if (spanCount == 2) gridLayoutManager.spanSizeLookup = getSpanSizeLookup(list)
+            layoutManager = gridLayoutManager
+            adapter = fileUploadAdapter
+        }
+        fileUploadAdapter.setSpanCount(spanCount)
+        fileUploadAdapter.submitList(list)
+    }
+
+    private fun getSpanSizeLookup(list: List<FileUploadModel>) = object : GridLayoutManager.SpanSizeLookup() {
+        override fun getSpanSize(position: Int): Int {
+            return if (list.size % 2 == 1 && position == list.lastIndex) 2 else 1
+        }
+    }
+
+    fun submitList(list: List<FileUploadModel>) {
+        fileUploadAdapter.submitList(list)
+    }
+
+    fun getCurrentList(): List<FileUploadModel> = fileUploadAdapter.currentList
+    fun onCurrentListChanged(previousList: List<FileUploadModel>, currentList: List<FileUploadModel>) {
+        fileUploadAdapter.onCurrentListChanged(previousList, currentList)
+    }
+
+    fun updateList(
+        item: FileUploadModel,
+        copy: (() -> FileUploadModel)
+    ) {
+        val oldList = getCurrentList()
+        val newList = oldList
+            .map { if (it.id == item.id) copy.invoke() else it.copy() }
+            .toMutableList()
+        onCurrentListChanged(oldList, newList)
+        submitList(newList)
+    }
+}
