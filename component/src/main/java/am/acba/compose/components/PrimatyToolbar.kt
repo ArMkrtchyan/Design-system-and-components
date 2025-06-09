c496c2c7f7161f84980097c2327f65b318dbd92e
Arshak Mkrtchyan <arshak.mkrtchyan@acba.am>
2025-06-09 14:08:21 +0400

    Add IDs to PrimaryToolbar components
    The following IDs have been added to the PrimaryToolbar:
    - `centre_title` for the title TextView.
    - `leftIcon` for the navigation ImageView.
    
    This change also includes setting test tags for the corresponding composable elements in the Jetpack Compose version of the PrimaryToolbar, mapping them to these Android resource IDs.
    
    Additionally, a new resource file `ids.xml` has been created to define these IDs.

--- component/src/main/java/am/acba/compose/components/PrimatyToolbar.kt
+++ component/src/main/java/am/acba/compose/components/PrimatyToolbar.kt
@@ -10,14 +10,18 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun PrimaryToolbar(
     title: String = "",
@@ -28,7 +32,11 @@ fun PrimaryToolbar(
         title = {
             PrimaryText(
                 modifier = Modifier
-                    .padding(end = 16.dp, start = 16.dp),
+                    .padding(end = 16.dp, start = 16.dp)
+                    .semantics {
+                        testTagsAsResourceId = true
+                    }
+                    .testTag("android:id/`centre_title`"),
                 text = title,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
@@ -42,7 +50,11 @@ fun PrimaryToolbar(
         navigationIcon = {
             IconButton(onClick = {
                 activity?.onBackPressed()
-            }) {
+            }, modifier = Modifier
+                .semantics {
+                    testTagsAsResourceId = true
+                }
+                .testTag("android:id/`leftIcon`")) {
                 PrimaryIcon(painterResource(R.drawable.ic_back))
             }
         }
