a7b4d46f322d74e550e9dd1efb21543e4187fd50
Edik.Harutyunyan <harutyunyanedik1992@gmail.com>
2025-07-01 17:23:23 +0400

    added extensions

--- component/src/main/java/am/acba/utils/extensions/NumberExtensions.kt
+++ component/src/main/java/am/acba/utils/extensions/NumberExtensions.kt
@@ -2,8 +2,8 @@
 
 fun Int?.orEmpty() = this ?: 0
 
-fun Long?.orEmpty() = this ?: 0
+fun Long?.orEmpty() = this ?: 0L
 
 fun Double?.orEmpty() = this ?: 0.0
 
-fun Float?.orEmpty() = this ?: 0f
+fun Float?.orEmpty() = this ?: 0F
