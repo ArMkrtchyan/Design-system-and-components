092f0c18adedab935eb0f68d1a6c370d397324ae
Arshak Mkrtchyan <arshak.mkrtchyan@acba.am>
2025-07-09 13:25:10 +0400

    Add screens and navigation for Data Table, File Upload, and Pie Chart components
    This commit introduces new screens and navigation entries for the Data Table, File Upload, and Pie Chart components.
    
    - Added `DataTableScreen.kt` and `DataTableComposeFragment.kt`.
    - Added `FileUploadScreen.kt` and `FileUploadComposeFragment.kt`.
    - Added `PieChartScreen.kt` and `PieChartComposeFragment.kt`.
    - Updated `ComponentTypeEnum.kt` to include entries for the new components.
    - Updated `navigation_graph.xml` to include fragments for the new components.
    - Marked `SLIDER` component as `isReady = true` in `ComponentTypeEnum.kt`.

---
+++ app/src/main/java/am/acba/composeComponents/piechart/PieChartScreen.kt
@@ -0,0 +1,55 @@
+﻿package am.acba.composeComponents.piechart
+
+import am.acba.compose.components.PrimaryToolbar
+import am.acba.compose.theme.DigitalTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PieChartScreen(title: String = "") {
+    Box(
+        modifier = Modifier
+            .background(DigitalTheme.colorScheme.backgroundBase)
+            .fillMaxSize()
+            .padding(
+                bottom = TopAppBarDefaults.windowInsets
+                    .only(WindowInsetsSides.Bottom)
+                    .asPaddingValues()
+                    .calculateBottomPadding()
+            )
+    ) {
+        Column(Modifier.fillMaxSize()) {
+            PrimaryToolbar(title = title)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp)
+                    .verticalScroll(rememberScrollState()),
+            ) {
+            }
+        }
+    }
+}
+
+@Composable
+@PreviewLightDark
+fun PieChartPreview() {
+    DigitalTheme {
+        PieChartScreen()
+    }
+}
