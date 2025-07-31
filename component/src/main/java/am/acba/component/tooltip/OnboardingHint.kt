993cf6a438b2e2f0c5dcf09d5f0965165e2dcee0
Hrayr.Amiraghyan <hrayr.amiraghyan@acba.am>
2025-07-31 19:03:07 +0400

    Fixed tooltip position

--- component/src/main/java/am/acba/component/tooltip/OnboardingHint.kt
+++ component/src/main/java/am/acba/component/tooltip/OnboardingHint.kt
@@ -180,7 +180,7 @@ class OnboardingHint(
                 viewX - x + view.width / 2 - binding.anchor.width / 2
             )
 
-            Pair(x, viewY - height - 64.dpToPx() + context.getStatusBarHeight())
+            Pair(x, viewY - height - 44.dpToPx() + context.getStatusBarHeight())
         } else {
             val x = calculateXCoordinateOfView(view)
             setAnchorPosition(
