18f1f4d77f7fec916cc542594179b309624212f3
Anna.Hovsepyan <Anna.HovsepyanM@acba.am>
2025-08-13 13:31:12 +0400

    Use TransparentButton.kt for handling input fields clicks

---
+++ component/src/main/java/am/acba/compose/common/TransparentButton.kt
@@ -0,0 +1,26 @@
+﻿package am.acba.compose.common
+
+import am.acba.compose.theme.ShapeTokens
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NonRestartableComposable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+@NonRestartableComposable
+fun TransparentButton(enabled: Boolean, modifier: Modifier, onClick: () -> Unit) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        enabled = enabled,
+        shape = ShapeTokens.shapePrimaryButton,
+        colors = ButtonColors(
+            contentColor = Color.Transparent,
+            containerColor = Color.Transparent,
+            disabledContentColor = Color.Transparent,
+            disabledContainerColor = Color.Transparent
+        )
+    ) {}
+}
