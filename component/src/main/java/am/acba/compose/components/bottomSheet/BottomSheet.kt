c39f3c2690930d1971d4203b42e22fefad073fcf
Anna.HovsepyanM <anna.yovsepean@gmail.com>
2025-12-03 18:01:08 +0400

    Fix bottom system navigation color

--- component/src/main/java/am/acba/compose/components/bottomSheet/BottomSheet.kt
+++ component/src/main/java/am/acba/compose/components/bottomSheet/BottomSheet.kt
@@ -16,10 +16,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
@@ -106,7 +108,8 @@ fun PrimaryBottomSheet(
                 },
                 properties = properties,
                 shape = ShapeTokens.shapeBottomSheet,
-                containerColor = DigitalTheme.colorScheme.backgroundTonal1
+                containerColor = DigitalTheme.colorScheme.backgroundTonal1,
+                contentWindowInsets = { WindowInsets.navigationBars }
             ) {
                 Column(
                     modifier = Modifier.onGloballyPositioned {
