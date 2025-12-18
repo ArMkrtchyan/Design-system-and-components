af47b74d0da1c16715ff1a567c6d7d620a96c29a
Anna.HovsepyanM <anna.yovsepean@gmail.com>
2025-12-18 17:10:07 +0400

    Add efes logo

--- component/src/main/java/am/acba/compose/theme/ThemedResourcesLight.kt
+++ component/src/main/java/am/acba/compose/theme/ThemedResourcesLight.kt
@@ -9,4 +9,5 @@ fun lightResourcesScheme(): ThemedResources = ThemedResources(
     arcaCardIcon = R.drawable.logo_arca_with_background_light,
     amexCardIcon = R.drawable.logo_amex_with_background_light,
     uPayCardIcon = R.drawable.logo_upay_with_background_light,
+    efesLogoIcon = R.drawable.ic_efes_logo_light,
 )
